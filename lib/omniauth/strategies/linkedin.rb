88dae33e9e1a690ffbfedc93d5230d30748481e0
Justiceshrs C. Portal <justiceshrsportal@gmail.com>
2022-11-06 15:20:58 +0800

    update state

--- lib/omniauth/strategies/linkedin.rb
+++ lib/omniauth/strategies/linkedin.rb
@@ -125,6 +125,17 @@ module OmniAuth
       def profile_endpoint
         "/v2/me?projection=(#{ fields.join(',') })"
       end
+
+      def authorize_params
+        params = super
+
+        provided_info = ['vanity_name', @env['rack.request.form_hash'].send(:[], 'vanity_name')].join(":")
+        state = [session["omniauth.state"], provided_info].join("~")
+        session["omniauth.state"] = state
+        params[:state] = state
+
+        params
+      end
     end
   end
 end
